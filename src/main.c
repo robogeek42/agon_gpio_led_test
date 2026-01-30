1f8aeb35842e48226663518817421a1e3b6a39af
assif <assif42@gmail.com>
2026-01-30 14:43:32 +0000

    Update to use IO macro defined in ez80f92.h

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include <time.h>
 
+#include <ez80f92.h>  // defines IO(addr) macro
+
 #define PB_DAT 0x009A
 #define PB_DIR 0x009B
 #define PC_DAT 0x009E
@@ -27,21 +29,6 @@ void delay(int timeout)
 	} while( timeout_ticks > clock() );
 }
 
-// Thanks to tomm8086 and calc84maniac on the Agon Discord
-// for the magic functions below
-
-// Read from a IO port
-static inline uint8_t io_in(int addr)
-{
-    return *((volatile uint8_t __attribute__((address_space(3))) *)addr);
-}
-
-// Read from a IO port
-static inline void io_out(int addr, uint8_t value)
-{
-    *((volatile uint8_t __attribute__((address_space(3))) *)addr) = value;
-}
-
 
 // MAIN
 int main(/*int argc, char *argv[]*/)
@@ -55,7 +42,7 @@ int main(/*int argc, char *argv[]*/)
 	struct keyboard_event_t e;
 
     // Set Bit 7 of PortD to be an output
-    io_out(PD_DIR, 0x7F);
+    IO(PD_DIR) = 0x7F;
     
     uint8_t state = 0;
 
@@ -65,13 +52,13 @@ int main(/*int argc, char *argv[]*/)
         {
             bquit = true;
         }
-        io_out(PD_DAT, state << 7);
+        IO(PD_DAT) = state << 7;
         state = 1 - state;
         delay(20);
     }
 
     // turn off the LED before exiting
-    io_out(PD_DAT, 0);
+    IO(PD_DAT) = 0;
 
     printf("\nBye.\n");
 
